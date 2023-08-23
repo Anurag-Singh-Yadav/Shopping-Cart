18ec9d3f9ecb20763350d7a37fa2f83f3b2df952
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2023-08-23 18:55:23 +0530

    loading

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
   }, []);
   return (
     <div className="max-w-[100vw] w-11/12 mx-auto pt-4 relative top-16">
-      {!loading ? (
+      {loading ? (
         <div className="flex justify-center items-center h-[10rem]"><Spinnner></Spinnner></div>
       ) : post.length > 0 ? (
         <div className="grid grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-6 pt-5">
